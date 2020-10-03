1b8af7605fafa5f633587115917676552cb734d0
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2020-10-04 00:36:34 +0200

    add dataset write function

--- dave/io_dave.py
+++ dave/io_dave.py
@@ -152,3 +152,100 @@ def read_dataset(dataset_path):
     else:
         print('Their is no suitable file at the given path')
 
+
+def write_dataset(grid_data, dataset_path):
+    """
+    This functions stores a dave dataset at a given path
+    """
+    # --- create file
+    archiv_file = pd.HDFStore(dataset_path)
+    # area
+    area = _convert_to_archiv_data(grid_data.area)
+    archiv_file.put('/area', area)
+    # target input
+    archiv_file.put('/target_input', grid_data.target_input)
+    # buildings
+    commercial = _convert_to_archiv_data(grid_data.buildings.commercial)
+    archiv_file.put('/buildings/commercial', commercial)
+    for_living = _convert_to_archiv_data(grid_data.buildings.for_living)
+    archiv_file.put('/buildings/for_living', for_living)
+    other = _convert_to_archiv_data(grid_data.buildings.other)
+    archiv_file.put('/buildings/other', other)
+    # roads
+    roads = _convert_to_archiv_data(grid_data.roads.roads)
+    archiv_file.put('/roads/roads', roads)
+    roads_plot = _convert_to_archiv_data(grid_data.roads.roads_plot)
+    archiv_file.put('/roads/roads_plot', roads_plot)
+    road_junctions = copy.deepcopy(grid_data.roads.road_junctions)
+    if not road_junctions.empty:
+        road_junctions = pd.DataFrame({'geometry': road_junctions})
+        road_junctions = convert_geometry_to_wkb(road_junctions)
+    else:
+        road_junctions = pd.DataFrame([])
+    archiv_file.put('/roads/road_junctions', road_junctions)
+    # landuse
+    landuse = _convert_to_archiv_data(grid_data.landuse)
+    archiv_file.put('/landuse', landuse)
+    # ehv data
+    ehv_lines = _convert_to_archiv_data(grid_data.ehv_data.ehv_lines)
+    archiv_file.put('/ehv_data/ehv_lines', ehv_lines)
+    ehv_nodes = _convert_to_archiv_data(grid_data.ehv_data.ehv_nodes)
+    archiv_file.put('/ehv_data/ehv_nodes', ehv_nodes)
+    ehv_substations = _convert_to_archiv_data(grid_data.ehv_data.ehv_substations)
+    archiv_file.put('/ehv_data/ehv_substations', ehv_substations)
+    # hv data
+    hv_nodes = _convert_to_archiv_data(grid_data.hv_data.hv_nodes)
+    archiv_file.put('/hv_data/hv_nodes', hv_nodes)
+    hv_lines = _convert_to_archiv_data(grid_data.hv_data.hv_lines)
+    archiv_file.put('/hv_data/hv_lines', hv_lines)
+    # mv data
+    mv_nodes = _convert_to_archiv_data(grid_data.mv_data.mv_nodes)
+    archiv_file.put('/mv_data/mv_nodes', mv_nodes)
+    mv_lines = _convert_to_archiv_data(grid_data.mv_data.mv_lines)
+    archiv_file.put('/mv_data/mv_lines', mv_lines)
+    # lv data
+    lv_nodes = _convert_to_archiv_data(grid_data.lv_data.lv_nodes)
+    archiv_file.put('/lv_data/lv_nodes', lv_nodes)
+    lv_lines = _convert_to_archiv_data(grid_data.lv_data.lv_lines)
+    archiv_file.put('/lv_data/lv_lines', lv_lines)
+    # components_power
+    conventional_powerplants = _convert_to_archiv_data(grid_data.components_power.conventional_powerplants)
+    archiv_file.put('/components_power/conventional_powerplants', conventional_powerplants)
+    renewable_powerplants = _convert_to_archiv_data(grid_data.components_power.renewable_powerplants)
+    archiv_file.put('/components_power/renewable_powerplants', renewable_powerplants)
+    loads = _convert_to_archiv_data(grid_data.components_power.loads)
+    archiv_file.put('/components_power/loads', loads)
+    ehv_ehv = _convert_to_archiv_data(grid_data.components_power.transformers.ehv_ehv)
+    archiv_file.put('/components_power/transformers/ehv_ehv', ehv_ehv)
+    ehv_hv = _convert_to_archiv_data(grid_data.components_power.transformers.ehv_hv)
+    archiv_file.put('/components_power/transformers/ehv_hv', ehv_hv)
+    hv_mv = _convert_to_archiv_data(grid_data.components_power.transformers.hv_mv)
+    archiv_file.put('/components_power/transformers/hv_mv', hv_mv)
+    mv_lv = _convert_to_archiv_data(grid_data.components_power.transformers.mv_lv)
+    archiv_file.put('/components_power/transformers/mv_lv', mv_lv)
+    # hp data
+    hp_junctions = _convert_to_archiv_data(grid_data.hp_data.hp_junctions)
+    archiv_file.put('/hp_data/hp_junctions', hp_junctions)
+    hp_pipes = _convert_to_archiv_data(grid_data.hp_data.hp_pipes)
+    archiv_file.put('/hp_data/hp_pipes', hp_pipes)
+    # mp data
+    mp_junctions = _convert_to_archiv_data(grid_data.mp_data.mp_junctions)
+    archiv_file.put('/mp_data/mp_junctions', mp_junctions)
+    mp_pipes = _convert_to_archiv_data(grid_data.mp_data.mp_pipes)
+    archiv_file.put('/mp_data/mp_pipes', mp_pipes)
+    # lp data
+    lp_junctions = _convert_to_archiv_data(grid_data.lp_data.lp_junctions)
+    archiv_file.put('/lp_data/lp_junctions', lp_junctions)
+    lp_pipes = _convert_to_archiv_data(grid_data.lp_data.lp_pipes)
+    archiv_file.put('/lp_data/lp_pipes', lp_pipes)
+    # components gas
+    sinks = _convert_to_archiv_data(grid_data.components_gas.sinks)
+    archiv_file.put('/components_gas/sinks', sinks)
+    sources = _convert_to_archiv_data(grid_data.components_gas.sources)
+    archiv_file.put('/components_gas/sources', sources)
+    # dave version
+    dave_version = pd.DataFrame({'dave_version': grid_data.dave_version}, index=[0])
+    archiv_file.put('/dave_version', dave_version)
+    # close file
+    archiv_file.close()
+
