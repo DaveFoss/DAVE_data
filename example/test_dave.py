f47ef1e8ea71cb73184330292f2c4fb86ce15061
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2021-01-12 14:54:14 +0100

    test script cleaned up

--- example/test_dave.py
+++ example/test_dave.py
@@ -1,6 +1,4 @@
 import os
-import timeit
-
 from dave.create import create_grid
 
 
@@ -8,19 +6,14 @@ from dave.create import create_grid
 This is a example file for testing dave
 """
 
-# start runtime
-_start_time = timeit.default_timer()
-
-
 ########## Examples for grid area definition #########################
 # test target by plz
-_postalcode_1 = ['34225']
+_postalcode_1 = ['34225']  # Baunatal
 _postalcode_2 = ['37085', '37075', '37083', '37079', '37081', '37073', '37077']  # Göttingen
 _postalcode_3 = ['ALL']
 
 # test target by town_name
-_town_name_1 = ['Baunatal']
-_town_name_11 = ['Göttingen']
+_town_name_1 = ['Göttingen']
 _town_name_2 = ['KAsSel', 'Baunatal']
 _town_name_3 = ['ALL']
 
@@ -28,72 +21,35 @@ _town_name_3 = ['ALL']
 _federal_state_1 = ['Hessen']
 _federal_state_2 = ['HeSsEn', 'SchleSWIg-HOLstein']
 _federal_state_3 = ['ALL']
-_federal_state_NB1 = ['Baden-Württemberg']  # Transnet BW Gebiet
-_federal_state_NB2 = ['Thüringen', 'Sachsen', 'Sachsen-Anhalt', 'Brandenburg',
-                      'Berlin', 'Mecklenburg-Vorpommern', 'Hamburg']  # 50 Hertz Gebiet
 
-# test own shape (Hertingshausen is a part from the Town Baunatal).
-# It has 500 relevant Buildings(for living and commercial))
+# test target by nuts-region
+_nuts_1 = ['DE']  # nuts level 0
+_nuts_2 = ['DE11A', 'DE11B']  # nuts level 3
+_nuts_3 = ['DEF', 'DE60']  # example for diffrent nuts level combined(1 and 2)
+
+
+# test own shape
+# Hertingshausen is a part from the Town Baunatal. (ca. 500 relevant Buildings)
 _own_area = os.path.dirname(os.path.realpath(__file__))+'\\hertingshausen\\hertingshausen.shp'
 
 
-##################### test with main function ##########################
+##################### test main function ##########################
 """
+This is the main function for DaVe with all possible parameters.
+For testing you can use the pre defined variables on the top or own ones.
+"""
+
 grid_data = create_grid(postalcode=None,
-                       town_name=None,
-                       federal_state=None,
-                       own_area=_own_area_tn,
-                       power_levels=['HV'],
-                       gas_levels=[],
-                       plot=False,
-                       convert = False)
-"""
-"""
-grid_data  = create_grid(postalcode=None,
-                                    town_name=None,
-                                    federal_state=['Hessen'],
-                                    own_area=None,
-                                    power_levels=['EHV', 'HV'],
-                                    gas_levels=[],
-                                    plot=False,
-                                    convert = False)
-"""
-"""
-grid_data, pp_model  = create_grid(postalcode=None,
-                                   town_name=None,
-                                   federal_state=None,
-                                   own_area=_own_area,
-                                   power_levels=['LV'],
-                                   gas_levels=[],
-                                   plot=False,
-                                   convert = True,
-                                   opt_model = False)
-"""
-#"""
-grid_data = create_grid(postalcode=None,
-                        town_name=_town_name_1,
+                        town_name=None,
                         federal_state=None,
-                        own_area=None,
-                        power_levels=['MV'],
+                        nuts_region=None,
+                        own_area=_own_area,
+                        power_levels=['LV'],
                         gas_levels=[],
-                        plot=False,
-                        convert=False,
-                        opt_model=False)
-#"""
-
-
-# stop and show runtime
-_stop_time = timeit.default_timer()
-print('runtime = ' + str(round((_stop_time - _start_time)/60, 2)) + 'min')
-
-"""
-# check time for special command
-# start runtime
-_start_time = timeit.default_timer()
-
-        grid_data.lv_data.lv_lines.at[line.name, 'to_bus'] = dave_name
-
-# stop and show runtime
-_stop_time = timeit.default_timer()
-print('runtime = ' + str(_stop_time - _start_time) + 'sek')
-"""
+                        plot=True,
+                        convert=False,  # if True a second return variable must be defined
+                        opt_model=False,
+                        transformers=False,
+                        renewable_powerplants=False,
+                        conventional_powerplants=False,
+                        loads=False)
