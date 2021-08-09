810e92d8d672a0066be72229c89c2b5238092c18
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2021-08-09 23:54:28 +0200

    added general convert function for geometric data

--- dave/io/convert_format.py
+++ dave/io/convert_format.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import geopandas as gpd
+import copy
 from shapely.wkb import loads, dumps
+
+from dave.dave_structure import davestructure
 
 
 def wkb_to_wkt(file, key):
@@ -15,12 +18,12 @@ def wkb_to_wkt(file, key):
     return data
 
 
-def wkt_to_wkb(data_key):
+def wkt_to_wkb(data_df):
     """
     This function converts a geometry data from WKT (geometric object) to WKB (hexadecimal string)
-    format for a given dataframe
+    format for a given geodataframe
     """
-    data = data_key.copy(deep=True)
+    data = data_df.copy(deep=True)
     if not data.empty:
         data = pd.DataFrame(data)
         if 'geometry' in data.keys():
@@ -28,3 +31,23 @@ def wkt_to_wkb(data_key):
     else:
         data = pd.DataFrame([])
     return data
+
+
+def wkt_to_wkb_dataset(grid_data):
+    """
+    This function converts all geometry data from WKT (geometric object) to WKB (hexadecimal string)
+    format for a given DaVe dataset
+    """
+    for key in grid_data.keys():
+        if isinstance(grid_data[key], davestructure):
+            for key_sec in grid_data[key].keys():
+                if isinstance(grid_data[key][key_sec], davestructure):
+                    for key_trd in grid_data[key][key_sec].keys():
+                        if isinstance(grid_data[key][key_sec][key_trd], gpd.GeoDataFrame):
+                            grid_data[key][key_sec][key_trd] = wkt_to_wkb(
+                                grid_data[key][key_sec][key_trd])
+                elif isinstance(grid_data[key][key_sec], gpd.GeoDataFrame):
+                    grid_data[key][key_sec] = wkt_to_wkb(grid_data[key][key_sec])
+        elif isinstance(grid_data[key], gpd.GeoDataFrame):
+            grid_data[key] = wkt_to_wkb(grid_data[key])
+    return grid_data
