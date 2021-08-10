6c097c036fc4f849e96dc73dda932db19ba0fcd6
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2021-08-10 10:54:14 +0200

    copy dataset at geometry converting

--- dave/io/convert_format.py
+++ dave/io/convert_format.py
@@ -38,16 +38,17 @@ def wkt_to_wkb_dataset(grid_data):
     This function converts all geometry data from WKT (geometric object) to WKB (hexadecimal string)
     format for a given DaVe dataset
     """
-    for key in grid_data.keys():
-        if isinstance(grid_data[key], davestructure):
-            for key_sec in grid_data[key].keys():
-                if isinstance(grid_data[key][key_sec], davestructure):
-                    for key_trd in grid_data[key][key_sec].keys():
-                        if isinstance(grid_data[key][key_sec][key_trd], gpd.GeoDataFrame):
-                            grid_data[key][key_sec][key_trd] = wkt_to_wkb(
-                                grid_data[key][key_sec][key_trd])
-                elif isinstance(grid_data[key][key_sec], gpd.GeoDataFrame):
-                    grid_data[key][key_sec] = wkt_to_wkb(grid_data[key][key_sec])
-        elif isinstance(grid_data[key], gpd.GeoDataFrame):
-            grid_data[key] = wkt_to_wkb(grid_data[key])
-    return grid_data
+    dataset = copy.deepcopy(grid_data)
+    for key in dataset.keys():
+        if isinstance(dataset[key], davestructure):
+            for key_sec in dataset[key].keys():
+                if isinstance(dataset[key][key_sec], davestructure):
+                    for key_trd in dataset[key][key_sec].keys():
+                        if isinstance(dataset[key][key_sec][key_trd], gpd.GeoDataFrame):
+                            dataset[key][key_sec][key_trd] = wkt_to_wkb(
+                                dataset[key][key_sec][key_trd])
+                elif isinstance(dataset[key][key_sec], gpd.GeoDataFrame):
+                    dataset[key][key_sec] = wkt_to_wkb(grid_data[key][key_sec])
+        elif isinstance(dataset[key], gpd.GeoDataFrame):
+            dataset[key] = wkt_to_wkb(grid_data[key])
+    return dataset
