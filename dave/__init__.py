36922c9bf7f85b4b43b32a4d97ee88d2c53ef894
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2021-09-06 11:35:19 +0200

    added pre-commit description

--- dave/__init__.py
+++ dave/__init__.py
@@ -1,8 +1,9 @@
-__version__ = '1.0.5'
+__version__ = "1.0.5"
 
 import os
+
 dave_dir = os.path.dirname(os.path.realpath(__file__))
-dave_output_dir = os.path.expanduser(r'~\Desktop\DaVe_output')
+dave_output_dir = os.path.expanduser(r"~\Desktop\DaVe_output")
 
 from dave.create import *
 from dave.dave_structure import *
