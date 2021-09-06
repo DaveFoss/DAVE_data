f543246b23cb1606ee9396f8a312a65c83808814
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2021-09-06 12:47:59 +0200

    changed code style

--- dave/topology/__init__.py
+++ dave/topology/__init__.py
@@ -1,10 +1,12 @@
 # general informations for target area
 from dave.topology.target_area import *
+
 # voltage levels for power grid
 from dave.topology.extra_high_voltage import *
 from dave.topology.high_voltage import *
 from dave.topology.medium_voltage import *
 from dave.topology.low_voltage import *
+
 # pressure levels for gas grid
 from dave.topology.high_pressure import *
 from dave.topology.medium_pressure import *
