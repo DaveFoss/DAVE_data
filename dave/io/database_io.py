bb2dab104f5cc9e74c83a6b5abd7658343a7efc5
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2021-09-13 00:59:24 +0200

    added geometry converting

--- dave/io/database_io.py
+++ dave/io/database_io.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 import pandas as pd
 from pymongo import GEOSPHERE, MongoClient
 from shapely.geometry import mapping, shape
+from shapely.wkt import loads
 
 from dave.settings import dave_settings
 
@@ -12,13 +13,15 @@ def from_mongo(database, collection, filter_method=None, geometry=None):
 
     INPUT:
         **filter_method** (string) - method for the geometrical data filtering
-        **geometry** (shapely object) - geometry for the filtering
+        **geometry** (string) - shapely geometry object as string for the filtering
     """
     # define data source
     client = MongoClient(
         f'mongodb://{dave_settings()["db_user"]}:{dave_settings()["db_pw"]}@{dave_settings()["db_ip"]}'
     )
     db = client[database]
+    # transform geometry from string to shapely object
+    geometry = loads(geometry)
     if (filter_method is not None) and (geometry is not None):
         # request data with geometrical filtering
         request = db[collection].find(
@@ -28,7 +31,7 @@ def from_mongo(database, collection, filter_method=None, geometry=None):
         # request all data from defined collection
         request = db[collection].find()
     data_list = list(request)
-    # transform geometries to shapely objects
+    # convert geometries to shapely objects
     for row in data_list:  #!!! add option for data with no geometry (convert to normal dataframe)
         row["geometry"] = shape(row["geometry"])
     if len(data_list) > 1:
