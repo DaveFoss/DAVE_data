7ca6c9d51f56fd4c1a78a6be963c59a8a7b2e30d
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2021-09-12 03:00:11 +0200

    added function for geometrical filtered request from mongo db

--- dave/datapool/requests.py
+++ dave/datapool/requests.py
@@ -101,25 +101,32 @@ def oep_request(schema, table, where=None, geometry=None):
 def mongo_request(database, collection, filter_method=None, geometry=None):
     """
     This function requests data from the mongo db
+
+    INPUT:
+        **filter_method** (string) - method for the geometrical data filtering
+        **geometry** (shapely object) - geometry for the filtering
     """
     # define data source
     client = MongoClient(
         f'mongodb://{dave_settings()["db_user"]}:{dave_settings()["db_pw"]}@{dave_settings()["db_ip"]}'
     )
     db = client[database]
-    if filter_method and geometry:
-        # add funtion for data request with filter option
-        pass
+    if (not filter_method is None) and (not geometry is None):
+        # request data with geometrical filtering
+        request = db[collection].find(
+            {"geometry": {f"${filter_method}": {"$geometry": mapping(geometry)}}}
+        )
     else:
         # request all data from defined collection
-        data_list = list(db[collection].find())
-        # transform geometries to shapely objects
-        for row in data_list:
-            row["geometry"] = shape(row["geometry"])
-        if len(data_list) > 0:
-            df = gpd.GeoDataFrame(data_list)
-        elif len(data_list) == 0:
-            df = gpd.GeoDataFrame([data_list[0]])
-
-        #!!! add option for data with no geometry (convert to normal dataframe)
+        request = db[collection].find()
+    data_list = list(request)
+    # transform geometries to shapely objects
+    for row in data_list:  #!!! add option for data with no geometry (convert to normal dataframe)
+        row["geometry"] = shape(row["geometry"])
+    if len(data_list) > 1:
+        df = gpd.GeoDataFrame(data_list)
+    elif len(data_list) == 1:
+        df = gpd.GeoDataFrame([data_list[0]])
+    else:
+        df = pd.DataFrame([])
     return df
