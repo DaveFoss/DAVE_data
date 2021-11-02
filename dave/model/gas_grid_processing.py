6fd75e32116d5bb028c09426a576a71f711c8653
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2021-11-03 00:58:47 +0100

    return grid with no edits

--- dave/model/gas_grid_processing.py
+++ dave/model/gas_grid_processing.py
@@ -1,7 +1,7 @@
 import pandapipes as ppi
 
 
-def gas_processing(net_power):
+def gas_processing(net_gas):
     """
     This function run a diagnosis of the pandapipes network and clean up occurring failures.
     Furthermore the grid will be adapt so all boundarys be respected.
@@ -12,6 +12,6 @@ def gas_processing(net_power):
     OUTPUT:
         **net** (attrdict) - A cleaned up and if necessary optimized pandapipes attrdict
     """
-    pass
+    return net_gas
     # hier wird das Gasnetzmodell nach dem es in pandapipes erstellt wurde, aufbereitet damit ein
     # lastfluss konvergiert und sonstige Fehler bereinigen
