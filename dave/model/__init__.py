a194de2e691fd32906d12a1297d72199ea4dd7d8
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2022-03-16 10:52:46 +0100

    added licence text

--- dave/model/__init__.py
+++ dave/model/__init__.py
@@ -2,6 +2,9 @@
 # Kassel and individual contributors (see AUTHORS file for details). All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+# converter
 from dave.model.create_pandapipes import *
 from dave.model.create_pandapower import *
+
+# general model functions
 from dave.model.model_utils import *
