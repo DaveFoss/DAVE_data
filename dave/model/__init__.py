59211a22f56a43a46e9bd68259f55d6b49b258d7
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2022-05-12 11:35:13 +0200

    rename mynts converter imports and scripts

--- dave/model/__init__.py
+++ dave/model/__init__.py
@@ -2,14 +2,13 @@
 # Kassel and individual contributors (see AUTHORS file for details). All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-from dave.model.Converter import *
-
 # converter
+from dave.model.converter import *
 from dave.model.create_gaslib import *
+from dave.model.create_mynts import *
 from dave.model.create_pandapipes import *
 from dave.model.create_pandapower import *
-from dave.model.Elements import *
+from dave.model.elements import *
 
 # general model functions
 from dave.model.model_utils import *
-from dave.model.MyntsWriter import *
