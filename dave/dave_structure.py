3dccfe578ee7724f75efb8ae28c3be84d83e1a26
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2023-06-14 13:06:06 +0200

    remove copy import

--- dave/dave_structure.py
+++ dave/dave_structure.py
@@ -1,9 +1,6 @@
 # Copyright (c) 2022-2023 by Fraunhofer Institute for Energy Economics and Energy System Technology (IEE)
 # Kassel and individual contributors (see AUTHORS file for details). All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-
-from copy import deepcopy
-
 import pandas as pd
 from pandapower.auxiliary import ADict
 
