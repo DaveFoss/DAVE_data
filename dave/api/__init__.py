212998514facceea28892b632fc4d882815baf0e
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2024-04-22 15:52:51 +0200

    adjusted copyright year

--- dave/api/__init__.py
+++ dave/api/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023 by Fraunhofer Institute for Energy Economics and Energy System Technology (IEE)
+# Copyright (c) 2022-2024 by Fraunhofer Institute for Energy Economics and Energy System Technology (IEE)
 # Kassel and individual contributors (see AUTHORS file for details). All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
