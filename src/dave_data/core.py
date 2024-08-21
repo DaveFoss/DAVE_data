92628fd232b8f6d1dab81a8c956ac2a78cc9d960
Krien <uwe.krien@ifam.fraunhofer.de>
2024-08-21 13:09:09 +0200

    Add core classes

--- src/dave_data/core.py
+++ src/dave_data/core.py
@@ -1,4 +1,75 @@
+import datetime
+
 import pandas as pd
+
+
+class MetaData:
+    """
+    Meta Data Class for DAVE_data.
+
+    Parameters
+    ----------
+    source_license
+    source_date
+
+
+    Attributes
+    ----------
+    license : str
+    organisation : str
+    source_date : str
+    fetch_date : datetime.date
+    source meta : dict
+    """
+
+    def __init__(self, source_license, source_date, organisation=None):
+        self.license = source_license
+        self.source_date = self._convert_date(source_date)
+        self.fetch_date = datetime.datetime.now()
+        self.source_url = None
+        self.organisation = None
+        self.source_meta = None
+
+    def _convert_date(self, value):
+        return ""
+
+
+class Data:
+    """
+    Attributes
+    ----------
+    name
+    """
+
+    def __init__(
+        self,
+        name,
+        description = None,
+        data=None,
+        meta=None,
+        polygon=None,
+        tags=None,
+    ):
+        """
+
+        Parameters
+        ----------
+        data : geopandas.geoDataFrame
+            Data table with the original data.
+        organ
+        """
+        self.name = name
+        self.description = description
+        self.data = data  # geopandas.geoDataFrame
+        self.meta = meta  # MetaData object
+        self.polygon = polygon  # Searching polygon
+        self.tags = tags  # list
+
+    def store(self):
+        pass
+
+    def restore(self):
+        pass
 
 
 def compute(args):
