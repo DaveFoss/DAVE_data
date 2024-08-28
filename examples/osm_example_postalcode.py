544169a2398603ab8f718bf59bded7a470e0bb84
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2024-08-29 00:55:33 +0200

    added example for requesting osm data by postalcode

---
+++ examples/osm_example_postalcode.py
@@ -0,0 +1,20 @@
+import contextily as cx
+from matplotlib import pyplot as plt
+
+from dave_data import osm_request
+from dave_data import postalcode_to_polygon
+
+# define area of interest by postalcode
+polygon = postalcode_to_polygon(["34225"])[0]
+print(polygon)
+
+# request streets for polygon
+streets = osm_request("road", polygon)
+
+# plot data
+print(streets.meta.organisation)
+streets_wm = streets.data.to_crs(epsg=3857)
+ax = streets_wm.plot(figsize=(8, 8), alpha=0.5, edgecolor="k")
+cx.add_basemap(ax, source=cx.providers.OpenStreetMap.DE)
+ax.set_axis_off()
+plt.show()
