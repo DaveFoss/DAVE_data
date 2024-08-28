b49e3a35111344c865e8d5526590d7cc979179aa
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2024-08-28 15:19:08 +0200

    added read functions to init file

--- src/dave_data/__init__.py
+++ src/dave_data/__init__.py
@@ -3,5 +3,15 @@ __version__ = "v0.0.1"
 from .core import compute
 from .core import get_data
 from .datapool.osm_request import osm_request
+from .datapool.read_data import read_federal_states
+from .datapool.read_data import read_nuts_regions
+from .datapool.read_data import read_postal
 
-__all__ = ["compute", "get_data", "osm_request"]
+__all__ = [
+    "compute",
+    "get_data",
+    "osm_request",
+    "read_postal",
+    "read_federal_states",
+    "read_nuts_regions",
+]
