8b6119503db811fcd6b08a4eaf496a749ddbd36f
Krien <uwe.krien@ifam.fraunhofer.de>
2024-08-30 00:06:43 +0200

    Extend example to Lod2

--- examples/lod_example.py
+++ examples/lod_example.py
@@ -15,14 +15,16 @@ from dave_data.datapool.lod import lod
 polygon = box(8.90, 53.16, 8.93, 53.15)
 # polygon = box(13.40, 52.51, 13.41, 52.52)
 
-lod = lod.get_lod(polygon)
+lod = lod.get_lod(polygon, n_lod=2)
 
 pprint.pprint(lod.meta)
 
 lod_map = lod.data
 
 lod_wm = lod_map.to_crs(epsg=3857)
-ax = lod_wm.plot(figsize=(8, 8), alpha=0.5, edgecolor="k")
+ax = lod_wm.plot(
+    figsize=(8, 8), alpha=0.5, edgecolor="k", column="HOEHEGEB", legend=True
+)
 cx.add_basemap(ax, source=cx.providers.OpenStreetMap.DE)
 ax.set_axis_off()
 streets = osm_request(
