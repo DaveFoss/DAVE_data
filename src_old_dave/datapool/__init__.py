e9a8a0e14db7857f5280b6eadc4bac304c8ee9d5
Krien <uwe.krien@ifam.fraunhofer.de>
2024-08-29 23:10:10 +0200

    Remove directory with old code

--- src_old_dave/datapool/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2022-2024 by Fraunhofer Institute for Energy Economics and Energy System Technology (IEE)
-# Kassel and individual contributors (see AUTHORS file for details). All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-
-from dave.datapool.oep_request import *
-from dave.datapool.osm_request import *
-from dave.datapool.read_data import *
