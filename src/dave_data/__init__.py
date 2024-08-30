cfc60ec83782aa184531e7fc6f7f064706ae4bb9
Krien <uwe.krien@ifam.fraunhofer.de>
2024-08-30 12:05:26 +0200

    Move area_to_polygon to subpackage geometry

--- src/dave_data/__init__.py
+++ src/dave_data/__init__.py
@@ -1,7 +1,23 @@
 __version__ = "v0.0.1"
 
+from dave_data.geometry.area_to_polygon import federal_state_to_polygon
+from dave_data.geometry.area_to_polygon import postalcode_to_polygon
+from dave_data.geometry.area_to_polygon import town_to_polygon
 from .core import compute
 from .core import get_data
 from .datapool.osm_request import osm_request
+from .datapool.read_data import read_federal_states
+from .datapool.read_data import read_nuts_regions
+from .datapool.read_data import read_postal
 
-__all__ = ["compute", "get_data", "osm_request"]
+__all__ = [
+    "postalcode_to_polygon",
+    "federal_state_to_polygon",
+    "town_to_polygon",
+    "compute",
+    "get_data",
+    "osm_request",
+    "read_postal",
+    "read_federal_states",
+    "read_nuts_regions",
+]
