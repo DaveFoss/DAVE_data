143b2746c846b3d3d7a3ebcb1d9739fa63de102a
Krien <uwe.krien@ifam.fraunhofer.de>
2024-08-30 12:52:00 +0200

    Fix sorting of imports

--- src/dave_data/__init__.py
+++ src/dave_data/__init__.py
@@ -1,13 +1,14 @@
 __version__ = "v0.0.1"
 
+from dave_data.core import compute
+from dave_data.core import get_data
+from dave_data.datapool import osm_request
 from dave_data.geometry.area_to_polygon import federal_state_to_polygon
 from dave_data.geometry.area_to_polygon import postalcode_to_polygon
 from dave_data.geometry.area_to_polygon import town_to_polygon
-
 from dave_data.io.read_data import read_federal_states
 from dave_data.io.read_data import read_nuts_regions
 from dave_data.io.read_data import read_postal
-
 
 __all__ = [
     "postalcode_to_polygon",
